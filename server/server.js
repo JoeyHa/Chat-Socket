827a965f2fca3340515836442b02b1d9edc60051
JoeyHa <joey.havia@gmail.com>
2018-05-12 17:23:59 +0300

    Added Socket.io + Connect/Disconnect handels

--- server/server.js
+++ server/server.js
@@ -1,13 +1,24 @@
 const express = require('express');
+const http = require('http');
 const path = require('path');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname,'../public');
 const port = process.env.PORT || 3000;
-const app = express();
-
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
+io.on('connection', (socket) => {
+    console.log('new User is connected');
 
-app.listen(3000,() => console.log(`App listening on port ${port}!`));
+    socket.on('disconnect',() => {
+        console.log('User was Disconnected');
+    });
+
+});
+
+server.listen(port,() => console.log(`App listening on port ${port}!`));
  
