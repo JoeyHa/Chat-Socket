d71afe8109fd816688218bbca5b84bd73987afb4
JoeyHa <joey.havia@gmail.com>
2018-05-14 22:32:20 +0300

    Added CSS layout

--- public/js/index.js
+++ public/js/index.js
@@ -16,14 +16,45 @@ socket.on('newMessage', function (message) {
     jQuery('#messages').append(li);
 });
 
+socket.on('newLocationMessage', function (message) {
+    var li = jQuery('<li></li>');
+    var a = jQuery('<a target= "_blank">My current Location</a>');
+
+    li.text(`${message.from}: `);
+    a.attr('href', message.url);
+    li.append(a);
+    jQuery('#messages').append(li);
+});
+
+var messageTextBox = jQuery('[name = message]');
 
 jQuery('#message-form').on('submit', function (e) {
     e.preventDefault();
 
-    socket.emit('createMessage', {
+    socket.emit('createMessage',   {
         from:'User',
-        text: jQuery('[name = message]').val()
+        text: messageTextBox.val()
     },function () {
+         messageTextBox.val('');
+    });
+});
 
+var locationButton = jQuery('#send-location');
+locationButton.on('click',function (){
+    if(!navigator.geolocation){
+        return alert('GeoLocation not supported by your browser.');
+    }
+
+    locationButton.attr('disabled', 'disabled').text('sending location..');
+    navigator.geolocation.getCurrentPosition(function (position) {
+        locationButton.removeAttr('disabled').text('Send Location');
+        socket.emit('createLocationMessage', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        });
+    }, function () {
+        locationButton.removeAttr('disabled').text('Send Location');
+        alert('Unable to fetch location');
+        
     });
 });
--- server/server.js
+++ server/server.js
@@ -3,7 +3,10 @@ const http = require('http');
 const path = require('path');
 const socketIO = require('socket.io');
 
-const {generateMessage} = require('./utils/message.js');
+const {
+    generateMessage,
+    generateLocationMessage} = require('./utils/message.js');
+    
 const publicPath = path.join(__dirname,'../public');
 const port = process.env.PORT || 3000;
 var app = express();
@@ -19,14 +22,14 @@ io.on('connection', (socket) => {
     socket.emit('newMessage', generateMessage('Admin','Welcome to chat app'));
     socket.broadcast.emit('newMessage', generateMessage('Admin' , 'New user joined'));
 
-
     socket.on('createMessage', (message,callback) => {
         console.log('created new message',message);
         io.emit('newMessage', generateMessage(message.from, message.text));
         callback('This is from the server .');
-   
     });
-
+    socket.on('createLocationMessage',(coords) => {
+        io.emit('newLocationMessage', generateLocationMessage('Admin',coords.latitude,coords.longitude));
+    });
     socket.on('disconnect',() => {
         console.log('User was Disconnected');
     });
